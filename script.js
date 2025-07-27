df58e4629875639a146a09735066bc469495d66e
Gabrielle Gervacio <140438379+gabriellengrvc@users.noreply.github.com>
2025-07-27 23:48:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -66,3 +66,7 @@ function toggleDarkMode() {
 window.addEventListener('load', function() {
     document.body.classList.add('loaded');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.classList.add('loaded');
+});
